f748a273e262da821567a1d84522ea525294bbd0
AJAYK-01 <ajayk@cet.ac.in>
2020-11-10 00:29:13 +0530

    7 steps added

---
+++ lib/steps/step5.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello World'),
+      ),
+      backgroundColor: Colors.green,
+      body: Center(
+        child: Container(
+          color: Colors.pink,
+          height: 70,
+          width: 200,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Text(
+                  'Name: John Doe',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Text(
+                  'Age: 54',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
